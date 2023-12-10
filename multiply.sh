8087276e2d7ad22c463f4e81c71ffc90ebe18382
nicholasSSUSE <nicholas.paolillo@suse.com>
2023-12-10 17:55:39 -0300

    100 charts

--- multiply.sh
+++ multiply.sh
@@ -7,10 +7,13 @@ src="charts/CH1"
 base="charts/CH"
 
 # The number of copies to make
-copies=15
+copies=100
+
+# The number of available icons
+icons=15
 
 # Start from 2 because CH1 already exists
-for (( i=3; i<=copies+1; i++ ))
+for (( i=2; i<=copies+1; i++ ))
 do
   # Construct the destination directory name
   dest="$base$i"
@@ -22,5 +25,13 @@ do
   chart_file="$dest/Chart.yaml"
 
   # Use sed to replace 'ch1' with the new chart name in the Chart.yaml file
-  sed -i "s/ch1/ch$i/g" "$chart_file"
+  # The pattern 'name: ch1' is used to ensure that only the chart name is replaced
+  sed -i "s/name: ch1/name: ch$i/g" "$chart_file"
+
+  # Calculate the icon number to use
+  icon_num=$(( (i-1) % icons + 1 ))
+
+  # Use sed to replace the icon number in the Chart.yaml file
+  sed -i "s/ch1.jpg/ch$icon_num.jpg/g" "$chart_file"
+
 done
